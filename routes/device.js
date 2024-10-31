29a91c7c0a5cf821b378b3d12773a638cd9efeb3
pkg4446 <pkg4446@gmail.com>
2024-10-31 14:29:23 +0900

    241031

--- routes/device.js
+++ routes/device.js
@@ -40,10 +40,15 @@ router.post('/log', async function(req, res) {
 });
 
 router.post('/hive_set', async function(req, res) {
-    const   path_device = "./data/device/"+req.body.DVC;
-    file_system.fileMK(""+path_device,req.body.TMP+","+req.body.RUN,"device_set.csv");
-
-    
+    const path_device  = "./data/device/"+req.body.DVC;
+    let   file_content = req.body.TMP+","+req.body.RUN;
+    file_system.fileMK(path_device,file_content,"device_set.csv");
+    file_content += ","+new Date()+"\r\n";
+    if(file_system.check(path_device+"/device_set_history.csv")){
+        file_system.fileADD(path_device,file_content,"device_set_history.csv");
+    }else{
+        file_system.fileMK(path_device,file_content,"device_set_history.csv");
+    }
     res.status(201).send("ack");
 });
 
