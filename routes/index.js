d5169635f46e9ce8753c0da9e006f40dec83d61a
pkg4446 <pkg4446@gmail.com>
2024-12-17 12:45:41 +0900

    241217

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.route("/")
     })
     .post(async function(req, res) {
         const response = {header:req.headers,body:req.body}
+        console.log(response);
         res.status(201).send(response);
     });
 router.use('/web',web);
